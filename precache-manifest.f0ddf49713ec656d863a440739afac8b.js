7cbd75c22dd8a2d4977da8ec4980071417d9811d
unknown <krishna2121.rana@gmail.com>
2018-10-26 15:04:19 +0200

    Updates

---
+++ precache-manifest.f0ddf49713ec656d863a440739afac8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1e59de7919b10f7340",
+    "url": "/hyf-react/homework-01/static/js/runtime~main.bc1e59de.js"
+  },
+  {
+    "revision": "6f3b0a187d0ef54061b3",
+    "url": "/hyf-react/homework-01/static/js/main.6f3b0a18.chunk.js"
+  },
+  {
+    "revision": "05798d51854879d0cc84",
+    "url": "/hyf-react/homework-01/static/js/1.05798d51.chunk.js"
+  },
+  {
+    "revision": "6f3b0a187d0ef54061b3",
+    "url": "/hyf-react/homework-01/static/css/main.f2b953ad.chunk.css"
+  },
+  {
+    "revision": "05798d51854879d0cc84",
+    "url": "/hyf-react/homework-01/static/css/1.08a533eb.chunk.css"
+  },
+  {
+    "revision": "4bd24f8ffe73b6fbf949be800c97702f",
+    "url": "/hyf-react/homework-01/index.html"
+  }
+];
